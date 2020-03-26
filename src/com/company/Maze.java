8e68ce2e976d024c1e7332aa3b8e8f3303201899
DESKTOP-6V2HAOJ\huadao <yang.sun@huadaocart.com>
2020-03-26 17:36:44 +0800

    company commit test

--- src/com/company/Maze.java
+++ src/com/company/Maze.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class Maze {
+
+    //v45
+    public static void main(String[] args) {
+        //初始化一个二维数组 模拟迷宫
+        int[][] map =new int[8][7];
+
+        // 模拟出墙
+
+
+    }
 }
